5a82da516c6dba81b31ef515641ffe0ed9c50ab1
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-11-02 10:29:06 -0300

    [std/optionals] add more gcc flags

--- std/optionals/test.sh
+++ std/optionals/test.sh
@@ -4,7 +4,7 @@ gcc -o test.exe test.c \
     optionals.c \
     ../unitc/unitc.c \
     -I"../../" \
-    -Wall -Wextra
+    -Wall -Wextra -Wpedantic -Wshadow
 
 echo
 echo "====~=~=~=~=~====~=~=~=~=~===="
